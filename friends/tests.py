454067620adcd9613d0eb6b69f9450ff003d13db
K02D <kirondeb02@gmail.com>
2024-03-19 03:34:09 -0400

    add tests

--- friends/tests.py
+++ friends/tests.py
@@ -10,8 +10,6 @@ from helpers.test.utils import (
 )
 from rest_framework.test import APITestCase
 from friends.views import send_friend_request, accept_friend_request, reject_friend_request
-# Create your tests here.
-
 
 class FriendRequestTest(APITestCase):
     def setUp(self):
@@ -19,33 +17,133 @@ class FriendRequestTest(APITestCase):
         self.student1 = create_student(user=self.user1)
         self.user2 = create_user(username="Bob", password="security2")
         self.student2 = create_student(user=self.user2)
+        self.user3 = create_user(username="Charlie", password="security3")
+        self.student3 = create_student(user=self.user3)
 
-        
+    def test_get_friends(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("get friends"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
+
+    def test_get_friends_with_data(self):
+        self.student1.friends.add(self.student2)
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("get friends"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_remove_friend(self):
+        self.student1.friends.add(self.student2)
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("remove friend", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.student1.friends.count(), 0)
+
+    def test_remove_nonexistent_friend(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("remove friend", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(self.student1.friends.count(), 0)
+
     def test_send_request(self):
         self.client.force_login(self.user1)
-        response = self.client.post(reverse("send friend request", args=[self.user1.id]))
-        self.assertEquals(response.status_code, 200)
-        self.assertEqual(1, len(FriendRequest.objects.all()))
-        print(response) 
-        
-    
+        response = self.client.post(reverse("send friend request", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 201)
+        self.assertEqual(FriendRequest.objects.count(), 1)
+        self.assertEqual(response.content.decode(), 'Friend request created')
+
+    def test_send_duplicate_request(self):
+        FriendRequest.objects.create(from_friend=self.student1, to_friend=self.student2)
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("send friend request", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(FriendRequest.objects.count(), 1)
+        self.assertEqual(response.content.decode(), 'Friend request already exists')
+
+    def test_withdraw_request(self):
+        FriendRequest.objects.create(from_friend=self.student1, to_friend=self.student2)
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("withdraw friend request", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertEqual(response.content.decode(), 'Friend request withdrawn')
+
+    def test_withdraw_nonexistent_request(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("withdraw friend request", args=[self.user2.id]))
+        self.assertEqual(response.status_code, 400)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertEqual(response.json()['error'], 'No friend request exists to withdraw')
+
+    def test_requests_sent(self):
+        FriendRequest.objects.create(from_friend=self.student1, to_friend=self.student2)
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("friend requests sent"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_requests_sent_empty(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("friend requests sent"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
+
+    def test_requests_received(self):
+        FriendRequest.objects.create(from_friend=self.student2, to_friend=self.student1)
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("friend requests received"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_requests_received_empty(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("friend requests received"))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
+
     def test_accept_request(self):
-        
-         self.client.force_login(self.user1)
-         friend_request = FriendRequest.objects.create(
-            from_friend=self.student1,
-            to_friend=self.student2
-        )
-         send = self.client.post(reverse("accept friend request", args=[friend_request.id]))
-         self.assertEquals(send.status_code, 200)
-         self.assertEqual(0, len(FriendRequest.objects.all()))
+        friend_request = FriendRequest.objects.create(from_friend=self.student2, to_friend=self.student1)
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("accept friend request", args=[friend_request.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertTrue(self.student1.friends.filter(id=self.student2.id).exists())
+        self.assertEqual(response.content.decode(), 'Friend request accepted')
+
+    def test_accept_nonexistent_request(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("accept friend request", args=[999]))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertFalse(self.student1.friends.filter(id=self.student2.id).exists())
+        self.assertEqual(response.json()['error'], 'Friend request not found')
 
     def test_reject_request(self):
-         self.client.force_login(self.user1)
-         friend_request = FriendRequest.objects.create(
-            from_friend=self.student1,
-            to_friend=self.student2
-        )
-         send = self.client.post(reverse("reject friend request", args=[friend_request.id]))
-         self.assertEquals(send.status_code, 200)
-         self.assertEqual(0, len(FriendRequest.objects.all()))
+        friend_request = FriendRequest.objects.create(from_friend=self.student2, to_friend=self.student1)
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("reject friend request", args=[friend_request.id]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertFalse(self.student1.friends.filter(id=self.student2.id).exists())
+        self.assertEqual(response.content.decode(), 'Friend request rejected')
+
+    def test_reject_nonexistent_request(self):
+        self.client.force_login(self.user1)
+        response = self.client.post(reverse("reject friend request", args=[999]))
+        self.assertEqual(response.status_code, 404)
+        self.assertEqual(FriendRequest.objects.count(), 0)
+        self.assertFalse(self.student1.friends.filter(id=self.student2.id).exists())
+        self.assertEqual(response.json()['error'], 'Friend request not found')
+
+    def test_search_friends(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("search friends", args=["Bob"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 1)
+
+    def test_search_friends_no_results(self):
+        self.client.force_login(self.user1)
+        response = self.client.get(reverse("search friends", args=["David"]))
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(len(response.json()), 0)
--- friends/views.py
+++ friends/views.py
@@ -30,14 +30,19 @@ def get_friends(request):
 
     return JsonResponse(friends, safe=False)
 
+
 def remove_friend(request, userId):
-    from_username = request.user
-    from_student = Student.objects.get(user__username=from_username)
-    to_student = Student.objects.get(user__id=userId)
-    # Remove the edge between friends
-    from_student.friends.remove(to_student)
-    to_student.friends.remove(from_student)
-    return HttpResponse('Friend removed', 200)
+    try:
+        from_username = request.user
+        from_student = Student.objects.get(user__username=from_username)
+        to_student = Student.objects.get(user__id=userId)
+        # Remove the edge between friends
+        from_student.friends.remove(to_student)
+        to_student.friends.remove(from_student)
+        return HttpResponse('Friend removed', 200)
+    except Student.DoesNotExist:
+        return JsonResponse({'error': 'Student not found'}, status=404)
+
 
 def search_friends(request, query):
     # Query users and prefetch related Student objects in a single query
@@ -46,6 +51,7 @@ def search_friends(request, query):
         Q(last_name__icontains=query) |
         Q(username__icontains=query)
     ).select_related('student').only('email', 'first_name', 'last_name', 'username', 'student__img_url')
+    
     friends = [
         {   
             'userId': user.id,
@@ -56,87 +62,105 @@ def search_friends(request, query):
             'img_url': getattr(user.student, 'img_url', None)  # Safely get img_url or None if student does not exist
         } for user in users_with_students if hasattr(user, 'student')
     ]
+    
     return JsonResponse(friends, safe=False)
 
 
 def send_friend_request(request, userId):
-    from_username = request.user
-    from_student = Student.objects.get(user__username=from_username)
-    to_student = Student.objects.get(user__id=userId)
-    friend_request , created = FriendRequest.objects.get_or_create(from_friend=from_student, to_friend=to_student)
- 
-    if created:
-        return HttpResponse('Friend request created', 201)
-    else:
-        return HttpResponse('Friend request failed', 400)
+    try:
+        from_username = request.user
+        from_student = Student.objects.get(user__username=from_username)
+        to_student = Student.objects.get(user__id=userId)
+        friend_request, created = FriendRequest.objects.get_or_create(from_friend=from_student, to_friend=to_student)
     
-# Gets all users the logged in user sent a friend request to
+        if created:
+            return HttpResponse('Friend request created', status=201)
+        else:
+            return HttpResponse('Friend request already exists', status=400)
+    except Student.DoesNotExist:
+        return JsonResponse({'error': 'Student not found'}, status=404)
+    
+
 def requests_sent(request):
-    from_username = request.user.username
-    from_student = Student.objects.get(user__username=from_username)
-    friend_requests = FriendRequest.objects.filter(from_friend=from_student).select_related('to_friend__user')
+    try:
+        from_username = request.user.username
+        from_student = Student.objects.get(user__username=from_username)
+        friend_requests = FriendRequest.objects.filter(from_friend=from_student).select_related('to_friend__user')
 
-    friends = [
-        {   
-            'friendRequestId': friend_request.id,  # Include the FriendRequest id
-            'userId': friend_request.to_friend.user.id,
-            'email': friend_request.to_friend.user.email,
-            'first_name': friend_request.to_friend.user.first_name,
-            'last_name': friend_request.to_friend.user.last_name,
-            'username': friend_request.to_friend.user.username,
-            'img_url': getattr(friend_request.to_friend, 'img_url', None)  # Safely get img_url or None if student does not exist
-        } for friend_request in friend_requests
-    ]
+        friends = [
+            {   
+                'friendRequestId': friend_request.id,  # Include the FriendRequest id
+                'userId': friend_request.to_friend.user.id,
+                'email': friend_request.to_friend.user.email,
+                'first_name': friend_request.to_friend.user.first_name,
+                'last_name': friend_request.to_friend.user.last_name,
+                'username': friend_request.to_friend.user.username,
+                'img_url': getattr(friend_request.to_friend, 'img_url', None)  # Safely get img_url or None if student does not exist
+            } for friend_request in friend_requests
+        ]
 
-    return JsonResponse(friends, safe=False)
+        return JsonResponse(friends, safe=False)
+    except Student.DoesNotExist:
+        return JsonResponse({'error': 'Student not found'}, status=404)
 
 
-# Returns requests sent to the logged in user
 def requests_received(request):
-    to_username = request.user.username
-    to_student = Student.objects.get(user__username=to_username)
-    # Get all requests sent to this student
-    friend_requests = FriendRequest.objects.filter(to_friend=to_student).select_related('from_friend__user')
-    
-    friends = [
-        {
-            'friendRequestId': friend_request.id,  # Include the FriendRequest ID
-            'userId': friend_request.from_friend.user.id,
-            'email': friend_request.from_friend.user.email,
-            'first_name': friend_request.from_friend.user.first_name,
-            'last_name': friend_request.from_friend.user.last_name,
-            'username': friend_request.from_friend.user.username,
-            'img_url': getattr(friend_request.from_friend, 'img_url', None)  # Safely get img_url or None
-        } for friend_request in friend_requests
-    ]
+    try:
+        to_username = request.user.username
+        to_student = Student.objects.get(user__username=to_username)
+        # Get all requests sent to this student
+        friend_requests = FriendRequest.objects.filter(to_friend=to_student).select_related('from_friend__user')
+        
+        friends = [
+            {
+                'friendRequestId': friend_request.id,  # Include the FriendRequest ID
+                'userId': friend_request.from_friend.user.id,
+                'email': friend_request.from_friend.user.email,
+                'first_name': friend_request.from_friend.user.first_name,
+                'last_name': friend_request.from_friend.user.last_name,
+                'username': friend_request.from_friend.user.username,
+                'img_url': getattr(friend_request.from_friend, 'img_url', None)  # Safely get img_url or None
+            } for friend_request in friend_requests
+        ]
 
-    return JsonResponse(friends, safe=False)
-
+        return JsonResponse(friends, safe=False)
+    except Student.DoesNotExist:
+        return JsonResponse({'error': 'Student not found'}, status=404)
 
 
 def withdraw_friend_request(request, userId):
-    from_username = request.user
-    from_student = Student.objects.get(user__username=from_username)
-    to_student = Student.objects.get(user__id=userId)
-    # Attempt to retrieve the existing friend request
     try:
+        from_username = request.user
+        from_student = Student.objects.get(user__username=from_username)
+        to_student = Student.objects.get(user__id=userId)
+        # Attempt to retrieve the existing friend request
         friend_request = FriendRequest.objects.get(from_friend=from_student, to_friend=to_student)
         friend_request.delete()
         return HttpResponse('Friend request withdrawn', 200)
+    except Student.DoesNotExist:
+        return JsonResponse({'error': 'Student not found'}, status=404)
     except FriendRequest.DoesNotExist:
         # If no friend request exists, return an error message
-        return HttpResponse('No friend request exists to withdraw', 400)
+        return JsonResponse({'error': 'No friend request exists to withdraw'}, status=400)
     
+
 def accept_friend_request(request, friendRequestId):
-    friend_request = FriendRequest.objects.get(id=friendRequestId)
-    # Create edge between friends
-    friend_request.to_friend.friends.add(friend_request.from_friend)
-    friend_request.from_friend.friends.add(friend_request.to_friend)
-    # Delete the friend request
-    friend_request.delete()
-    return HttpResponse('Friend request accepted', 200)
+    try:
+        friend_request = FriendRequest.objects.get(id=friendRequestId)
+        # Create edge between friends
+        friend_request.to_friend.friends.add(friend_request.from_friend)
+        friend_request.from_friend.friends.add(friend_request.to_friend)
+        # Delete the friend request
+        friend_request.delete()
+        return HttpResponse('Friend request accepted', 200)
+    except FriendRequest.DoesNotExist:
+        return JsonResponse({'error': 'Friend request not found'}, status=404)
     
+
 def reject_friend_request(request, friendRequestId):
-    friend_request = FriendRequest.objects.get(id=friendRequestId)
-    friend_request.delete()
-    return HttpResponse('Friend request rejected', 200)
+    try:
+        friend_request = FriendRequest.objects.get(id=friendRequestId)
+        friend_request.delete()
+        return HttpResponse('Friend request rejected', 200)
+    except FriendRequest.DoesNotExist:
+        return JsonResponse({'error': 'Friend request not found'}, status=404)
