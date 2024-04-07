6a66ef8de5bedc56d09beec2e04585828ed7866c
Tiger Ding <zding27@jh.edu>
2024-04-07 16:37:06 -0400

    Fix: make tslint happy

--- static/js/redux/ui/SideBar.tsx
+++ static/js/redux/ui/SideBar.tsx
@@ -231,7 +231,7 @@ const SideBar = () => {
     masterSlots = (
       <div className="empty-state">
         <img
-          src={curTheme.name == 'light' ? "/static/img/emptystates/masterslots.png" : "/static/img/emptystates/masterslots-dark.png"}
+          src={curTheme.name === 'light' ? "/static/img/emptystates/masterslots.png" : "/static/img/emptystates/masterslots-dark.png"}
           alt="No courses added."
         />
         <h4>Looks like you don&#39;t have any courses yet!</h4>
