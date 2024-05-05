047a7d20a68c50a9dde2b177da9039cc9192acf7
tigerding <zhiyuanding01@gmail.com>
2024-05-05 19:35:29 +0000

    refractor: add types in endpoints

--- static/js/redux/constants/endpoints.ts
+++ static/js/redux/constants/endpoints.ts
@@ -12,59 +12,62 @@ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
 GNU General Public License for more details.
 */
 
+import { User } from "../ui/modals/PeerModalComponents/Types";
+import { Course, Semester, Slot, Timetable } from "./commonTypes";
+
 /* server endpoints */
 export const getLogiCalEndpoint = () => "/user/log_ical/";
-export const getCourseInfoEndpoint = (courseId, semester) =>
+export const getCourseInfoEndpoint = (courseId: Course['id'], semester: string) =>
   `/courses/${semester}/id/${courseId}/`;
-export const getCourseSearchEndpoint = (query, semester, page = 1, limit = 6) =>
+export const getCourseSearchEndpoint = (query: string, semester: string, page = 1, limit = 6) =>
   `/search/${semester}/${query}/?page=${page}&limit=${limit}`;
 export const getTimetablesEndpoint = () => "/timetables/";
-export const getLoadSavedTimetablesEndpoint = (semester) =>
+export const getLoadSavedTimetablesEndpoint = (semester: Semester) =>
   `/user/timetables/${semester.name}/${semester.year}/`;
 export const getSaveTimetableEndpoint = () => "/user/timetables/";
 export const getPersonalEventEndpoint = () => "/user/events/";
-export const getDeleteTimetableEndpoint = (semester, name) =>
+export const getDeleteTimetableEndpoint = (semester: Semester, name: string) =>
   `/user/timetables/${semester.name}/${semester.year}/${name}/`;
-export const getTimetablePreferencesEndpoint = (id) =>
+export const getTimetablePreferencesEndpoint = (id: Timetable['id']) =>
   `/user/timetables/${id}/preferences/`;
 export const getSaveSettingsEndpoint = () => "/user/settings/";
-export const getClassmatesEndpoint = (semester, courses) =>
+export const getClassmatesEndpoint = (semester: Semester, courses: Array<Slot['course']>) =>
   `/user/classmates/${semester.name}/${semester.year}?${$.param({
     course_ids: courses,
   })}`;
-export const getClassmatesInCourseEndpoint = (school, semester, courseId) =>
+export const getClassmatesInCourseEndpoint = (school: string, semester: string, courseId: Course['id']) =>
   `/course_classmates/${school}/${semester}/id/${courseId}/`;
-export const getMostClassmatesCountEndpoint = (semester, courses) =>
+export const getMostClassmatesCountEndpoint = (semester: Semester, courses: Array<Slot['course']>) =>
   `/user/classmates/${semester.name}/${semester.year}?${$.param({
     course_ids: courses,
     count: true,
   })}`;
-export const getFriendsEndpoint = (semester) =>
+export const getFriendsEndpoint = (semester: Semester) =>
   `/user/classmates/${semester.name}/${semester.year}/`;
 
 // Friends endpoints
 export const getFetchFriendsEndpointEndpoint = () => `/friends/`;
-export const getRemoveFriendEndpoint = (userId) => `/friends/remove/${userId}`;
-export const getSearchFriendsEndpoint = (query) => `/friends/search/${query}`;
-export const getSendFriendRequestEndpoint = (userId) =>
+export const getRemoveFriendEndpoint = (userId: User['userId']) => `/friends/remove/${userId}`;
+export const getSearchFriendsEndpoint = (query: string) => `/friends/search/${query}`;
+export const getSendFriendRequestEndpoint = (userId: User['userId']) =>
   `/friends/send_request/${userId}`;
 export const getFriendRequestsSentEndpoint = () => `/friends/requests_sent`;
 export const getFriendRequestsReceivedEndpoint = () => `friends/requests_received`;
-export const getAcceptFriendRequestEndpoint = (friendRequestId) =>
+export const getAcceptFriendRequestEndpoint = (friendRequestId: number) =>
   `/friends/accept_request/${friendRequestId}`;
-export const getRejectFriendRequestEndpoint = (friendRequestId) =>
+export const getRejectFriendRequestEndpoint = (friendRequestId: number) =>
   `/friends/reject_request/${friendRequestId}`;
 
-export const getSchoolInfoEndpoint = (school) => `/school/${school}/`;
+export const getSchoolInfoEndpoint = (school: string) => `/school/${school}/`;
 export const getReactToCourseEndpoint = () => "/user/reactions/";
 export const getRequestShareTimetableLinkEndpoint = () => "/timetables/links/";
 export const acceptTOSEndpoint = () => "/tos/accept/";
-export function getCourseShareLinkFromModal(code, semester) {
+export function getCourseShareLinkFromModal(code: Course['code'], semester: Semester) {
   return `/course/${encodeURIComponent(code)}/${semester.name}/${semester.year}`;
 }
 // TODO: ${window.location.href.split('/')[2]} insert above ^
 
-export function getCourseShareLink(code, semester) {
+export function getCourseShareLink(code: Course['code'], semester: Semester) {
   return `/course/${encodeURIComponent(code)}/${semester.name}/${semester.year}`;
 }
 
